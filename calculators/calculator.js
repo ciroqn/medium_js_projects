96c9a321c705deabf1bacfa76d0fc3ea628224c9
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2022-10-14 09:14:44 +0100

    upload the html and js

---
+++ calculators/calculator.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/bmiCalculator", function(req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/", function(req, res) {
+  console.log(req.body);
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+  let result = num1 + num2;
+  res.send("The result of the calculation is " + result);
+});
+
+app.post("/bmiCalculator", function(req, res) {
+  let height = parseFloat(req.body.height);
+  let weight = parseFloat(req.body.weight);
+  let result = weight / height**2;
+  res.send("Your BMI is " + result);
+});
+
+app.listen(3000, function() {
+  console.log("Server starting on port 3000");
+});
