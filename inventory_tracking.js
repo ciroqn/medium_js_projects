874947ea53aaee8d8a0df13e9ace7060b63d30e7
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-01-29 20:39:32 +0000

    Update inventory_tracking.js

--- inventory_tracking.js
+++ inventory_tracking.js
@@ -76,3 +76,28 @@ class Film extends Media {
   }
 };
 
+// example of instances + testing
+
+const historyOfEverything = new Book("Bill Bryson", "A Short History of Nearly Everything", 544);
+
+historyOfEverything.toggleCheckOutStatus();
+
+console.log(historyOfEverything.isCheckedOut);
+
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+
+console.log(historyOfEverything.getAverageRating());
+
+const speed = new Film("Jan de Bont", "Speed", 116);
+
+speed.toggleCheckOutStatus();
+
+console.log(speed.isCheckedOut);
+
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+
+console.log(speed.getAverageRating());
