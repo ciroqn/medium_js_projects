49c6f9ac29d9d926e946a3b252924d238117d3c3
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-01-29 17:35:20 +0000

    instantiate Film class

--- inventory_tracking.js
+++ inventory_tracking.js
@@ -58,3 +58,21 @@ class Book extends Media {
     return this._pages;
   }
 };
+
+// instantiate Film class
+class Film extends Media {
+  constructor(title, director, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+
+  get director() {
+    return this._director;
+  }
+
+  get runTime() {
+    return this._runTime;
+  }
+};
+
