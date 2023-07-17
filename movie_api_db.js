212af9e301578c558f5606ec0782c07d70b58db9
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-07-17 14:47:58 +0100

    Create movie_api_db.js and write functions

---
+++ movie_api_db.js
@@ -0,0 +1,43 @@
+const tmdbKey = '<api_key>';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+const playBtn = document.getElementById('playBtn');
+
+const getGenres = async () => {
+  const genereRequestEndpoint = '/genre/movie/list';
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${genereRequestEndpoint}${requestParams}`;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await repsonse.json();
+      console.log(jsonResponse);
+      // genres property of response object
+      const genres = jsonResponse.genres;
+      console.log(genres);
+      return genres;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getMovies = () => {
+  const selectedGenre = getSelectedGenre();
+
+};
+
+const getMovieInfo = () => {
+
+};
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie = () => {
+  const movieInfo = document.getElementById('movieInfo');
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie();
+  };
+
+};
+
+getGenres().then(populateGenreDropdown);
+playBtn.onclick = showRandomMovie;
