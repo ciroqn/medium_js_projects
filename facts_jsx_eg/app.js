99064dd0717cb1b1a8a6be8ab720b20ab78a2805
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2024-02-28 23:11:18 +0000

    add functionality

--- facts_jsx_eg/app.js
+++ facts_jsx_eg/app.js
@@ -5,6 +5,14 @@ import { createRoot } from "react-dom/client";
 const container = document.getElementById("app");
 const root = createRoot(container);
 
+const displayFact = (e) => {
+  const animal = e.target.alt;
+  const randNum = Math.floor(Math.random() * animals[animal].facts.length);
+  const funFact = animals[animal].facts[randNum];
+  // add fun fact to screen
+  document.getElementById("fact").innerHTML(funFact);
+};
+
 const title = "";
 const background = (
   <img className="background" alt="ocean" src="/images/ocean.jpg" />
@@ -29,6 +37,7 @@ const animalFacts = (
     <h1>{title === "" ? "Click an animal for a fun fact" : title}</h1>;
     {background}
     <div className="animals">{images}</div>
+    <p id="fact"></p>
   </div>
 );
 
