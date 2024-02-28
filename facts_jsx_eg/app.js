df1c69fe900e8d5cebe275ba24895bab11d98204
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2024-02-28 22:34:16 +0000

    Create app.js

---
+++ facts_jsx_eg/app.js
@@ -0,0 +1,18 @@
+import { animals } from "./animals";
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+
+const title = "";
+const background = (
+  <img className="background" alt="ocean" src="/images/ocean.jpg" />
+);
+const animalFacts = (
+  <div>
+    <h1>{title === "" ? "Click an animal for a fun fact" : title}</h1>;
+  </div>
+);
+
+root.render(animalFacts);
