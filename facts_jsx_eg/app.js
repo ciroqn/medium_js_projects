e32c933a9416dbc2bab01ecf0ce4bf882932bfcd
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2024-02-28 22:49:24 +0000

    Update app.js

--- facts_jsx_eg/app.js
+++ facts_jsx_eg/app.js
@@ -9,6 +9,23 @@ const title = "";
 const background = (
   <img className="background" alt="ocean" src="/images/ocean.jpg" />
 );
+
+// create array of images tp be displayed
+const images = [];
+for (const animal in animals) {
+  images.push(
+    <img
+      key={animal}
+      className="animal"
+      alt={animal}
+      src={animals[animal].image}
+      aria-label={animal}
+      role="button"
+    />
+  );
+}
+
+
 const animalFacts = (
   <div>
     <h1>{title === "" ? "Click an animal for a fun fact" : title}</h1>;
