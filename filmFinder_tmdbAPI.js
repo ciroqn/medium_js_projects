da658d87edd9231552bca1198421e70c4cdaa2fa
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2024-02-28 14:04:06 +0000

    Create filmFinder_tmdbAPI.js

---
+++ filmFinder_tmdbAPI.js
@@ -0,0 +1,48 @@
+const tmdbKey = 'apiKey';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3';
+const playBtn = document.getElementById('playBtn');
+
+// GET fetch -- gets genres from TMDB using genre URL (see docs on TMDB)
+const getGenres = async () => {
+  // genre endpoint
+  const genreRequestEndpoint = tmdbBaseUrl + '/genre/movie/list';
+  const requestParams = '?api_key=' + tmdbKey;
+  // construct url to pass to fetch()
+  const urlToFetch = genreRequestEndpoint + requestParams;
+
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      // convert to JSON obj
+      const jsonResponse = await response.json();
+      // check if working
+      // console.log(jsonResponse);
+      // save genres property
+      const genres = jsonResponse.genres;
+      return genres;
+    }
+  } catch(err) {
+    console.log(err);
+  }
+};
+
+const getMovies = () => {
+  const selectedGenre = getSelectedGenre();
+
+};
+
+const getMovieInfo = () => {
+
+};
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie = () => {
+  const movieInfo = document.getElementById('movieInfo');
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie();
+  };
+
+};
+
+getGenres().then(populateGenreDropdown);
+playBtn.onclick = showRandomMovie;
